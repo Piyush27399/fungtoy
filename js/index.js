58190d1dec828c7cab6424f82ed946b0f3312b88
jwong <jenn.ew@gmail.com>
2014-03-11 23:36:03 -0700

    more scroll animation

--- js/index.js
+++ js/index.js
@@ -6,3 +6,19 @@ function scrollDown(name) {
 $('#adventure-link').click(function() {
 	scrollDown('adventure');
 });
+
+$('#wedding-party-link').click(function() {
+	scrollDown('wedding-party');
+});
+
+$('#wedding-details-link').click(function() {
+	scrollDown('wedding-details');
+});
+
+$('#registry-link').click(function() {
+	scrollDown('registry');
+});
+
+$('#contact-link').click(function() {
+	scrollDown('contact');
+});
