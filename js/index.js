f9f66aeabad848d9e10a498fbd334fc115084537
jwong <jenn.ew@gmail.com>
2014-03-31 22:11:17 -0700

    setting responsive in js function

--- js/index.js
+++ js/index.js
@@ -30,16 +30,25 @@ $('#contact-link').click(function() {
 function contentReposition() {
 	var $mainDiv = $('.l-main-div');
 	var $screenHeight = $(window).height();
+	var $screenWidth = $(window).width();
 	var $mainContentHeight = $('#l-main-content').outerHeight(true);
 	var marginTop = ($screenHeight - $mainContentHeight) / 2;
 
-	if (marginTop > 30 && $mainContentHeight <= $screenHeight) {
+	if (marginTop > 30 && $mainContentHeight <= $screenHeight && $screenWidth > 480) {
 		$mainDiv.css({
 			"padding-top": marginTop,
 			"padding-bottom": marginTop,
 			"padding-left": "30px",
 			"padding-right": "30px"
 		});
+	}
+	else if (marginTop > 30 && $mainContentHeight <= $screenHeight && $screenWidth <= 480) {
+		$mainDiv.css({
+			"padding-top": marginTop,
+			"padding-bottom": marginTop,
+			"padding-left": "10px",
+			"padding-right": "10px"
+		});		
 	}
 	else if (marginTop < 30) {
 		$mainDiv.css("padding", "30px");
