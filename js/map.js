f5976087bc7b7d192a840b94a36e8fc5bd5c92ea
jwong <jenn.ew@gmail.com>
2014-04-08 23:06:07 -0700

    adding directions to google map

--- js/map.js
+++ js/map.js
@@ -44,16 +44,20 @@ function initMap() {
     map.setOptions({styles: styles});
 
     var locations = [
-        ['Shakespeare Garden', 37.768682, -122.466783, 'img/wedding-date.png', 'https://goo.gl/maps/4zoew'],
-        ['Academy of Science', 37.769979, -122.466288, 'img/wedding-glasses.png', 'https://goo.gl/maps/D1PnJ'],
-        ['Grand Hyatt San Francisco', 37.78913, -122.40725, 'img/hotel.png', 'https://goo.gl/maps/qcwdt'],
-        ['Westin SFO', 37.604182, -122.376023, 'img/hotel.png', 'https://goo.gl/maps/iFVnp'],
-        ['Sutter & Stockton Parking', 37.78956, -122.4068945, 'img/wedding-parking.png', 'https://goo.gl/maps/MqP6M']
+        ['Shakespeare Garden', 37.768682, -122.466783, 'img/wedding-date.png', 'Martin Luther King Jr Dr', 'San Francisco, CA 94118'],
+        ['Academy of Science', 37.769979, -122.466288, 'img/wedding-glasses.png', '55 Music Concourse Dr', 'San Francisco, CA 94118'],
+        ['Grand Hyatt San Francisco', 37.78913, -122.40725, 'img/hotel.png', '345 Stockton St', 'San Francisco, CA 94108'],
+        ['The Westin San Francisco Airport', 37.604182, -122.376023, 'img/hotel.png', '444 Stockton St', 'San Francisco, CA 94108'],
+        ['Sutter & Stockton Parking', 37.78956, -122.4068945, 'img/wedding-parking.png', '1 Old Bayshore Hwy', 'Millbrae, CA 94030']
     ]
 
     var marker;
     var markers = [];
     var infowindow = new google.maps.InfoWindow();
+
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsService = new google.maps.DirectionsService();
+    directionsDisplay.setMap(map);
 
     for (var i = 0; i < locations.length; i++) {
         var location = locations[i];
@@ -63,10 +67,50 @@ function initMap() {
             map: map,
             icon: img
         });
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        google.maps.event.addListener(marker, 'click', (function(mark, i) {
             return function() {
-                infowindow.setContent(locations[i][0]);
-                infowindow.open(map, marker);
+                var contentDiv = document.createElement('div');
+                var title = document.createElement('h3');
+                title.innerHTML = locations[i][0];
+
+                var address = document.createElement('p');
+                address.innerHTML = locations[i][4] + "<br/>" + locations[i][5];
+
+                var starting = document.createElement('span');
+                starting.innerHTML = 'Start &nbsp;';                
+
+                var input = document.createElement('input');
+                input.setAttribute('type', 'textbox');
+
+                var directions = document.createElement('p');
+                var directionsLink = document.createElement('a');
+                directionsLink.innerHTML = 'Get Directions!';
+                directions.appendChild(directionsLink);
+
+                contentDiv.appendChild(title);
+                contentDiv.appendChild(address);
+                contentDiv.appendChild(starting);
+                contentDiv.appendChild(input);
+                contentDiv.appendChild(directions);
+                contentDiv.appendChild(document.createElement("br"));
+
+                directionsLink.addEventListener('click', function() {
+                    var start = input.value;
+                    var request = {
+                        origin: start,
+                        destination: locations[i][4] + ", " + locations[i][5],
+                        travelMode: google.maps.TravelMode.DRIVING
+                    };
+                    directionsService.route(request, function(result, status) {
+                        if (status == google.maps.DirectionsStatus.OK) {
+                            directionsDisplay.setDirections(result);
+                        }
+                    });
+                    infowindow.close();
+                });
+
+                infowindow.setContent(contentDiv);
+                infowindow.open(map, mark);
             }
         })(marker, i));
     }
